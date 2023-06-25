3d5c4ff4b7c0f180165e366816c2a50e4ac0876e
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 08:27:29 +0200

    with variable

--- src/components/example_blue_ball.ts
+++ src/components/example_blue_ball.ts
@@ -1,10 +1,12 @@
 import './example_blue_ball.scss';
 import { wrapAsExample } from './wrapAsExample';
 
+const description = 'This shows how to use <code>border-radius: 50%</code> to create a round shape.';
+
 export const example_blue_ball = () => {
 	let html = '';
 	html += `
 		<div class="ball">the ball</div>	
 	`;
-	return wrapAsExample('example_blue_ball', html, 'This shows how to use <code>border-radius: 50%</code> to create a round shape.');
+	return wrapAsExample('example_blue_ball', html, description);
 }
