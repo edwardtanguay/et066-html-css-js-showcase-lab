884c72ed5c63f10405beeb43ab2529625990bd89
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 07:32:36 +0200

    basic blue ball in

---
+++ src/components/example_blue_ball/index.ts
@@ -0,0 +1,7 @@
+import './style.scss';
+
+export const example_blue_ball = () => {
+	return `
+		<div class="ball">the ball</div>	
+	`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './styles/main.scss';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
 <h1>Info Site</h1>
 <p>Welcome to this site.</p>
 `;
